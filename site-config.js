178869b2f3296bbfa594935dc45639b01c0b7165
Rafaela Malta <rafaelamalta@Rafaelas-MacBook-Pro.local>
2026-06-11 18:32:58 -0700

    Add Safari donate page for FindOut app

--- site-config.js
+++ site-config.js
@@ -5,6 +5,8 @@ window.FINDOUT_SITE = {
   siteUrl: 'https://findoutfund.org',
   contactEmail: 'findoutfund@gmail.com',
   supportEmail: 'findoutfund@gmail.com',
+  supabaseUrl: 'https://mzafhovczzzyjqleajkk.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16YWZob3Zjenp6eWpxbGVhamtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNTc0ODksImV4cCI6MjA5MzczMzQ4OX0.fZOl9cNggJtoZoNjzKWubEROhdUmTk1VKBUSzbpKJi4',
   testflightUrl: '',
   appStoreUrl: '',
   stripeSupportUrl: '',
